c3b947320d38a59bcf1614790a494bff51c05110
Jonathan Felipe <jonathankuss@hotmail.com>
2025-06-22 17:34:14 -0300

    Configuração do firebase

--- App.js
+++ App.js
@@ -1,38 +1,23 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './navigation/AppNavigator';
-import AuthNavigator from './navigation/AuthNavigator';
-import { auth } from './firebase';
-import { onAuthStateChanged } from 'firebase/auth';
-import { View, ActivityIndicator } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
+// Futuras telas: FeedScreen, ProfileScreen...
+
+const Stack = createStackNavigator();
 
 export default function App() {
-
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const handleAuthChange = (currentUser) => {
-            setUser(currentUser);
-            setLoading(false);
-        };
-
-        const unsubscribe = onAuthStateChanged(auth, handleAuthChange);
-
-        return unsubscribe;
-    }, []);
-
-    if (loading) {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center' }}>
-                <ActivityIndicator size="large" />
-            </View>
-        )
-    }
-
-    return (
-        <NavigationContainer>
-            {user ? <AppNavigator /> : <AuthNavigator />}
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+        {/* Stack.Screen name="Feed" component={FeedScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
